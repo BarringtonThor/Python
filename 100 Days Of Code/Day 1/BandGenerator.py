102b9abbcf789a725dcbc251e41e083e7ea4662e
Barry Thorpe <Barringtonthorpe@gmail.com>
2023-04-06 23:58:31 -0500

    Add files via upload

---
+++ 100 Days Of Code/Day 1/BandGenerator.py
@@ -0,0 +1,7 @@
+print("Welcome to the Band Name Generator.")
+print("****************************************************")
+city = input("What's the name of the city you grew up in?\n")
+print("****************************************************")
+pet = input("What's the name of your pet?\n")
+print("****************************************************")
+print("Your band name could be " + city + " " + pet)
---
+++ 100 Days Of Code/Day 1/input.py
@@ -0,0 +1,11 @@
+# print("Hello " + input("What is your name?") + "!")
+
+# print(len(input("What is your name? ")))
+
+name = input("What is your name? ")
+print(len(name))
+
+
+name = input("What is your name? ")
+length = len(name)
+print(length)
