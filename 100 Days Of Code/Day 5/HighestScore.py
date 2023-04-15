c4b715a4ed0fe4e6955bcedece390bf757f0cb22
Barry Thorpe <Barringtonthorpe@gmail.com>
2023-04-14 22:56:00 -0500

    Add files via upload

---
+++ 100 Days Of Code/Day 5/HighestScore.py
@@ -0,0 +1,20 @@
+student_scores = input("Input a list of students scores! ")
+
+for n in range(0, len(student_scores)):
+    student_scores[n] = int(student_scores[n])
+print(student_scores)
+
+print(max(student_scores))
+print(min(student_scores))
+
+highest_score = 0
+for score in student_scores:
+    if score > highest_score:
+        highest_score = score
+print(f"The hightest score in the class is: {score}")
+
+lowest_score = 0
+for score in student_scores:
+    if score < lowest_score:
+        lowest_score = score
+print(f"The lowest score in the class is: {score}")
